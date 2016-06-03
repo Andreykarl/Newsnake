ff34ce1ff538a50bd96aebf1e85f09d3f41d399e
Jeff Walker <arioh.belskiy@gmail.com>
2016-06-03 12:24:36 +0600

    p2

--- NewSnake/NewSnake/Program.cs
+++ NewSnake/NewSnake/Program.cs
@@ -16,6 +16,12 @@ namespace NewSnake
             p1.sym = '%';
             p1.Draw();
 
+            Point p2 = new Point();
+            p2.x = 6;
+            p2.y = 9;
+            p2.sym = '$';
+            p2.Draw();
+
             Console.ReadLine();
         }
     }
